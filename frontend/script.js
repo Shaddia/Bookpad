547c69744bc911d3c670db5475df0ea3002d8338
Shaddia <79724581+Shaddia@users.noreply.github.com>
2024-08-27 00:27:47 +0000

    Mongodb added

---
+++ frontend/script.js
@@ -0,0 +1,26 @@
+document.getElementById('registerForm').addEventListener('submit', async (e) => {
+    e.preventDefault();
+    
+    const firstName = document.getElementById('firstName').value;
+    const lastName = document.getElementById('lastName').value;
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+  
+    try {
+      console.log("Intentando1")
+      const response = await fetch('http://localhost:5501/api/auth/register', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ firstName, lastName, email, password }),
+      });
+      console.log("Intentandoq")
+      const data = await response.json();
+      document.getElementById('responseMessage').textContent = data.msg;
+  
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  });
+  
